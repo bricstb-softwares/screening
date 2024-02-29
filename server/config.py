2eae56a2479bc3ceb7dadd3c8072ce87d852d711
jodafons <jodafons@lps.ufrj.br>
2024-02-28 23:38:10 -0300

    up

--- server/config.py
+++ server/config.py
@@ -5,12 +5,14 @@ import os
 #
 # server configuration
 #
+default_model_path="/mnt/brics_data/models/v1/user.philipp.gaspar.convnets_v1.baseline.shenzhen_santacasa.exp.20240207.r1/job.test_0.sort_0/output.pkl"
+model_path = os.environ.get("MODEL_PATH",default_model_path)
 
 class server_flags:
 
     # NOTE: This is a test path
     #model    = "/mnt/brics_data/models/user.philipp.gaspar.convnets.baseline.shenzhen_santacasa.exp.989f87bed5.r1/job.test_0.sort_0/output.pkl"
-    model    = "/mnt/brics_data/models/v1/user.philipp.gaspar.convnets_v1.baseline.shenzhen_santacasa.exp.20240207.r1/job.test_0.sort_0/output.pkl"
+    model    = model_path
     log_path = os.getcwd() + '/output.log'
 
 
