19750a9a135f030b2cc0a2cf592468ccc51349a5
jodafons <jodafons@lps.ufrj.br>
2024-03-17 18:43:28 -0300

    up

--- server/config.py
+++ server/config.py
@@ -18,6 +18,9 @@ class server_flags:
 
 
 def update_log( path=server_flags.log_path ):
+    if not os.path.exists(path):
+        with open(path,'w') as f:
+            pass
     with open(path, "r") as f:
         return f.read()
 
